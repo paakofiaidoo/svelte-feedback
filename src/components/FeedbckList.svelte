5fe0de57c55053eac8587903d30b88fde11b53be
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-12-12 21:06:04 +0000

    dded ainmaations

--- src/components/FeedbckList.svelte
+++ src/components/FeedbckList.svelte
@@ -1,11 +1,14 @@
 <script>
+  import { fade, scale } from "svelte/transition";
   import FeedbackItem from "./FeedbackItem.svelte";
   export let feedback = [];
 </script>
 
 <!-- markup (zero or more items) goes here -->
 {#each feedback as fb (fb.id)}
-  <FeedbackItem item={fb} on:delete-feedback />
+  <div in:scale out:fade>
+    <FeedbackItem item={fb} on:delete-feedback />
+  </div>
 {/each}
 
 <style>
